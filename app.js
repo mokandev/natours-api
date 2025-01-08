e55666efaa3425d147c2e17c9f4e8c73cbddd587
Carlos Afonso <carlos.flach@rdstation.com>
2025-01-08 16:49:19 -0300

    feat: handling GET requests

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello From the Server', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
