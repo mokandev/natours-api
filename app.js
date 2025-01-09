5f86a8d3f579f4ce9461a2fe19d2fefc4203cec7
Carlos Afonso <carlos.flach@rdstation.com>
2025-01-09 18:23:54 -0300

    feat: Handling POST Requests

--- app.js
+++ app.js
@@ -3,16 +3,41 @@ const express = require('express');
 
 const app = express();
 
+// Passing a middleware to app to read req body.
+app.use(express.json());
+
 const PORT = 8000;
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
 app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const tour = req.body;
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, tour);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 app.listen(PORT, () => {
