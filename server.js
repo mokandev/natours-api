a8409cf60e5a6928ae604d444093d1c5388f8164
Carlos Afonso <carlos.flach@rdstation.com>
2025-01-10 23:02:42 -0300

    feat: reading env vars from config.env

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
+const PORT = process.env.PORT || 3000;
+
 // 4) START SERVER
-const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
