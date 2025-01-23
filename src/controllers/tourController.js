11ca2b528ce50875e67165d6fe1030bc49276f68
Carlos Afonso <carlos.flach@rdstation.com>
2025-01-23 12:06:10 -0300

    feat: deleting documents

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -51,11 +51,13 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
+    return res.status(204).json({ status: "success"})
+  } catch (error) {
+    return res.status(400).json({ status: 'fail', message: error.message });
+  }
 };
 
 module.exports = {
