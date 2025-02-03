efe74a5d45f9e0438d6c68baa4bddae3c4184439
Carlos Afonso <carlos.flach@rdstation.com>
2025-02-03 11:02:37 -0300

    feat: Handling Unhandled Routes

--- src/app.js
+++ src/app.js
@@ -13,11 +13,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello From the Middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -30,5 +25,16 @@ const userRouter = require('./routes/userRoutes');
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+
+// ERROR HANDLER MUST BE DEFINED AFTER OTHER HANDLERS, SO IF THE OTHER ROUTES DONT MATCH IT, IT WILL BE CALLED.
+app.all('*', (req, res) => {
+  res
+    .status(404)
+    .json({
+      status: 'fail',
+      message: `Can't find ${req.originalUrl} on this server!`,
+    });
+});
 
 module.exports = app;
