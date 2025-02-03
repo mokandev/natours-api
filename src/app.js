a8b7aa8ab100be48f65defd3260176fdcfce6f4f
Carlos Afonso <carlos.flach@rdstation.com>
2025-02-03 15:03:12 -0300

    feat: global error handling middleware

--- src/app.js
+++ src/app.js
@@ -26,15 +26,17 @@ const userRouter = require('./routes/userRoutes');
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
 
-
-app.all('*', (req, res) => {
-  res
-    .status(404)
-    .json({
-      status: 'fail',
-      message: `Can't find ${req.originalUrl} on this server!`,
-    });
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
 });
 
 module.exports = app;
