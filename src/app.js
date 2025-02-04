793c4c7092910ee3808d7ed722324a35367299a9
Carlos Afonso <carlos.flach@rdstation.com>
2025-02-04 17:50:11 -0300

    feat: Better Errors and Refactoring

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -17,26 +21,13 @@ app.use((req, res, next) => {
   next();
 });
 
-// 3) ROUTES
-
-// HERE IS WHERE WE MOUNT THE ROUTES
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
