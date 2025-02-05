f61a2a5a0ee9b7759a702d034eafc5428227001b
Carlos Afonso <carlos.flach@rdstation.com>
2025-02-05 11:04:32 -0300

    feat: Handling Duplicate Database Fields

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -6,6 +6,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   return res.status(err.statusCode).json({
     status: err.status,
@@ -42,6 +49,9 @@ const errorController = (err, req, res, next) => {
 
     if (err.name === 'CastError') {
       error = handleCastErrorDB(err);
+    }
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(err);
     }
     sendErrorProd(error, res);
   }
