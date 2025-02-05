64bc5e0d18417f5381441ed75e7ff6bcba308428
Carlos Afonso <carlos.flach@rdstation.com>
2025-02-05 10:25:38 -0300

    feat: Errors during development vs production

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,7 +1,36 @@
+const sendErrorDev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res
+      .status(err.statusCode)
+      .json({ status: err.status, message: err.message });
+  } else {
+    // Programming or other unknown error: don't leak error details
+    console.error('ERROR', err);
+    return res
+      .status(500)
+      .json({ status: 'error', message: 'Something went very wrong!' });
+  }
+};
+
 const errorController = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else {
+    sendErrorProd(err, res);
+  }
 };
 
 module.exports = errorController;
