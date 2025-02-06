10709b6fb5755447e4a2bf62a2ed9d0300366afd
Carlos Afonso <carlos.flach@rdstation.com>
2025-02-06 15:13:41 -0300

    feat: Errors outside Express - Unhandled Rejections

--- src/server.js
+++ src/server.js
@@ -22,6 +22,16 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 // 4) START SERVER
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+
+  console.log('UNHANDLED REJECTIONS! Shutting down...');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
