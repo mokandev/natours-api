83325120afddacbb8664b0cbe045c3cff5388428
Carlos Afonso <carlos.flach15@gmail.com>
2025-02-15 18:23:20 -0300

    feat: Protecting Tour Routes Part 1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -47,4 +47,32 @@ const login = catchAsync(async (req, res, next) => {
   return res.status(200).json({ status: 'success', token });
 });
 
-module.exports = { signup, login };
+const protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it's there
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401),
+    );
+  }
+  
+  // 2) Verification token
+
+
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+
+  next();
+});
+
+module.exports = { signup, login, protect };
--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 // Destructuring
 // const {
 //   getAllTours,
@@ -11,17 +12,19 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/tour-stats').get(tourController.getTourStats)
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 // Param Middleware
 // router.param('id', tourController.checkID);
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
