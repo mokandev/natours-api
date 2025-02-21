ab0c1ca10dc5ca390110a7b56a3cad15e7cf9a4e
Carlos Afonso <carlos.flach@rdstation.com>
2025-02-21 16:17:51 -0300

    feat: password reset functionality - Setting new password

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -60,6 +60,14 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+
+  next();
+});
+
 userSchema.methods.correctsPassword = async function (
   candidatePassword,
   userPassword,
