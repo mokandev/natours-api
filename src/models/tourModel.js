8b872bde487d98d4fc7b67c1544ccad5e348495d
Carlos Afonso <carlos.flach@rdstation.com>
2025-05-27 16:59:05 -0300

    feat: embedding tour guides within a tour

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -76,6 +77,31 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      }
+    ], 
+    guides: Array
   },
   {
     toJSON: {
@@ -93,6 +119,13 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// Embeding tour guides within a tour.
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id))
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// })
 
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save a document...');
